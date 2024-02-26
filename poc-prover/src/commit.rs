f788d545c41b9247a6d6ab30e240cc40ec756631
flyq <flyq951@gmail.com>
2024-02-26 20:07:33 +0800

    fix poly

--- poc-prover/src/commit.rs
+++ poc-prover/src/commit.rs
@@ -1,26 +1,34 @@
 use ark_bn254::{Bn254, Fr, G1Affine, G1Projective, G2Affine};
 use ark_ec::{pairing::Pairing, AffineRepr, CurveGroup, VariableBaseMSM};
-use ark_ff::PrimeField;
+use ark_ff::{Field, PrimeField};
 use ark_poly::{univariate::DensePolynomial, DenseUVPolynomial, Polynomial};
 
 use crate::poly::vanish;
 
 pub struct CommitmentScheme {
     poly: DensePolynomial<Fr>, // for quicker msm operation
+    w: Fr,                     // root of unity
     srs_g1: Vec<G1Affine>,
     srs_g2: Vec<G2Affine>,
 }
 
 impl CommitmentScheme {
-    pub fn setup(poly: DensePolynomial<Fr>, srs_g1: Vec<G1Affine>, srs_g2: Vec<G2Affine>) -> Self {
+    // poly's length must be 2^k
+    pub fn setup(
+        poly: DensePolynomial<Fr>,
+        w: Fr,
+        srs_g1: Vec<G1Affine>,
+        srs_g2: Vec<G2Affine>,
+    ) -> Self {
         assert_eq!(
-            poly.len(),
+            poly.coeffs().len(),
             srs_g1.len(),
             "poly length is greater than srs_g1 length"
         );
 
         CommitmentScheme {
             poly,
+            w,
             srs_g1,
             srs_g2,
         }
@@ -30,15 +38,17 @@ impl CommitmentScheme {
         commit(&self.poly, &self.srs_g1)
     }
 
-    pub fn open(&self, x: Fr) -> (Fr, G1Affine) {
+    pub fn open(&self, x_idx: u64) -> (Fr, G1Affine) {
+        let x = self.w.pow([x_idx]);
         let y = self.poly.evaluate(&x);
-        let vanishing_poly = vanish(&self.poly, x, y);
+        let vanishing_poly = vanish(&self.poly, x_idx, y, self.w);
         let point = commit(&vanishing_poly, &self.srs_g1);
         (y, point)
     }
 
     // e(C- yG1, G2) = e(H, \tau G2 - xG2)
-    pub fn verify(&self, commit: G1Affine, y: Fr, proof: G1Affine, x: Fr) -> bool {
+    pub fn verify(&self, commit: G1Affine, y: Fr, proof: G1Affine, x_idx: u64) -> bool {
+        let x = self.w.pow([x_idx]);
         let y_g1: G1Affine = (G1Affine::generator() * y).into();
         let lhs = Bn254::pairing(commit - y_g1, G2Affine::generator());
 
@@ -49,10 +59,61 @@ impl CommitmentScheme {
 }
 
 pub fn commit(poly: &DensePolynomial<Fr>, srs_g1: &[G1Affine]) -> G1Affine {
+    // use std::ops::Mul;
+
+    // poly.coeffs()
+    //     .iter()
+    //     .zip(srs_g1.iter())
+    //     .fold(G1Affine::identity(), |acc, (a_i, g_i)| {
+    //         (acc + g_i.mul(a_i)).into()
+    //     })
+
     let poly = poly
         .coeffs()
         .iter()
         .map(|fr| fr.into_bigint())
         .collect::<Vec<_>>();
     <G1Projective as VariableBaseMSM>::msm_bigint(srs_g1, &poly).into_affine()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::poly::poly;
+
+    fn new_srs() -> (Vec<G1Affine>, Vec<G2Affine>) {
+        let x = vec![1, 3, 9, 27, 81, 243, 729, 2187];
+        let x = x.iter().map(|x| Fr::from(*x)).collect::<Vec<Fr>>();
+        let g1 = x
+            .iter()
+            .map(|x| (G1Affine::generator() * x).into())
+            .collect::<Vec<G1Affine>>();
+        let g2 = vec![G2Affine::generator(), (G2Affine::generator() * x[1]).into()];
+        (g1, g2)
+    }
+
+    #[test]
+    fn test_commitment_scheme() {
+        let points = vec![
+            Fr::from(12u64),
+            Fr::from(123u64),
+            Fr::from(1234u64),
+            Fr::from(12345u64),
+            Fr::from(123456u64),
+            Fr::from(1234567u64),
+            Fr::from(12345678u64),
+            Fr::from(123456789u64),
+        ];
+
+        let (f, w) = poly(points).unwrap();
+        let cs = CommitmentScheme::setup(f, w, new_srs().0, new_srs().1);
+
+        let commit = cs.commit();
+
+        for i in 0..8 {
+            let (y, proof) = cs.open(i);
+
+            assert!(cs.verify(commit, y, proof, i))
+        }
+    }
 }
--- poc-prover/src/poly.rs
+++ poc-prover/src/poly.rs
@@ -1,44 +1,95 @@
 use ark_bn254::Fr;
+use ark_ff::Field;
 use ark_ff::One;
 use ark_poly::{
     univariate::DensePolynomial, DenseUVPolynomial, EvaluationDomain, Evaluations,
-    GeneralEvaluationDomain, Radix2EvaluationDomain,
+    GeneralEvaluationDomain,
 };
 use std::ops::{Div, Sub};
 
 use crate::error::{Error, Result};
 
-// given [y1, y2, ..., yn], Returns the f(x) with (n-1) degree passing through points:
-// (1, y1), (2, y2), ..., (n, yn)
-pub fn poly(points: Vec<Fr>) -> Result<DensePolynomial<Fr>> {
-    let domain = GeneralEvaluationDomain::new(points.len())
+// given [y0, y1, y3, ..., y_n-1], Returns the f(x) with (n-1) degree passing through points:
+// (w^0, y0), (w^1, y1), ..., (w^n-1, y_n-1)
+// n must < subfield size
+pub fn poly(points: Vec<Fr>) -> Result<(DensePolynomial<Fr>, Fr)> {
+    let domain = GeneralEvaluationDomain::<Fr>::new(points.len())
         .ok_or(Error::PolyError("no poly".to_string()))?;
 
     let res = Evaluations::from_vec_and_domain(points, domain).interpolate();
-    Ok(res)
+    Ok((res, domain.element(1)))
 }
 
-// calculate q(x) = (f(x) - yi) / (x - i)
-pub fn vanish(f: &DensePolynomial<Fr>, i: Fr, y_i: Fr) -> DensePolynomial<Fr> {
+// calculate q(x) = (f(x) - yi) / (x - w^i)
+pub fn vanish(f: &DensePolynomial<Fr>, i: u64, y_i: Fr, w: Fr) -> DensePolynomial<Fr> {
     let q = f.sub(&DensePolynomial::from_coefficients_vec(vec![y_i]));
 
-    let x = DensePolynomial::from_coefficients_vec(vec![-i, Fr::one()]);
+    let x = DensePolynomial::from_coefficients_vec(vec![-w.pow([i]), Fr::one()]);
     q.div(&x)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ark_ff::Field;
     use ark_poly::Polynomial;
+    use std::str::FromStr;
 
     #[test]
     fn test_poly() {
-        let points = vec![Fr::from(6u64), Fr::from(12u64), Fr::from(20u64)];
-        let f = poly(points).unwrap();
+        let points = vec![
+            Fr::from(3u64),
+            Fr::from(7u64),
+            Fr::from(23u64),
+            Fr::from(24u64),
+        ];
+        let (f, w) = poly(points).unwrap();
+        println!("w: {:?}", w.to_string());
+        assert_eq!(f.evaluate(&Fr::from(1u64)), Fr::from(3u64));
+        assert_eq!(f.evaluate(&w), Fr::from(7u64));
+        assert_eq!(f.evaluate(&w.pow([2])), Fr::from(23u64));
+        assert_eq!(f.evaluate(&w.pow([3])), Fr::from(24u64));
+    }
 
-        println!("{:?}", f.coeffs());
-        assert_eq!(f.evaluate(&Fr::from(1u64)), Fr::from(6u64));
-        assert_eq!(f.evaluate(&Fr::from(2u64)), Fr::from(12u64));
-        assert_eq!(f.evaluate(&Fr::from(3u64)), Fr::from(20u64));
+    // sage code:
+    // r = 21888242871839275222246405745257275088548364400416034343698204186575808495617
+    // Fr = GF(r)
+    // w = Fr(21888242871839275217838484774961031246007050428528088939761107053157389710902)
+    // R.<x> = PolynomialRing(Fr, 'x')
+    // points = [(1,3),(w,7),(w^2,23), (w^3, 24)]
+    // f = R.lagrange_polynomial(points)
+    // p = (f-23)/(x-w^2)
+    // p
+    // 5472060717959818824295265560073355102937675480627776552657213863672231958938*x^2 +
+    // 21888242871839275203512741621498238757747780019892266376965541369547528660582*x +
+    // 16416182153879456416684804308942956316411273300312025757773653139931856371704
+    #[test]
+    fn test_vanish() {
+        let points = vec![
+            Fr::from(3u64),
+            Fr::from(7u64),
+            Fr::from(23u64),
+            Fr::from(24u64),
+        ];
+        let (f, w) = poly(points).unwrap();
+
+        let q = vanish(&f, 2, Fr::from(23u64), w);
+        assert_eq!(
+            q,
+            DensePolynomial::from_coefficients_vec(vec![
+                Fr::from_str(
+                    "16416182153879456416684804308942956316411273300312025757773653139931856371704"
+                )
+                .unwrap(),
+                Fr::from_str(
+                    "21888242871839275203512741621498238757747780019892266376965541369547528660582"
+                )
+                .unwrap(),
+                Fr::from_str(
+                    "5472060717959818824295265560073355102937675480627776552657213863672231958938"
+                )
+                .unwrap(),
+            ])
+        );
     }
 }
