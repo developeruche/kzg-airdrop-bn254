d33d5b357a3914a90e33799fe5a0070ff5ccd70b
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-02-28 11:11:01 +0100

    rm:> .idea

--- poc-prover/src/data.rs
+++ poc-prover/src/data.rs
@@ -7,16 +7,64 @@ use ethers::{
     utils::keccak256,
 };
 use std::str::FromStr;
-
 use crate::error::{Error, Result};
+use std::fs;
+use serde::{Deserialize, Serialize};
 
-pub fn srs_g1() -> Vec<G1Affine> {
-    vec![]
+
+
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct SRSRaw {
+    transcripts: Vec<Transcripts>,
+    participant_ids: Vec<String>,
+    participant_ecdsa_signatures: Vec<String>
 }
 
-pub fn srs_g2() -> Vec<G2Affine> {
-    vec![]
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Transcripts {
+    num_g1_powers: u64,
+    num_g2_powers: u64,
+    powers_of_tau: PowersOfTau,
+    witness: Witness
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct PowersOfTau {
+    g1_powers: Vec<String>,
+    g2_powers: Vec<String>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Witness {
+    running_products: Vec<String>,
+    pot_pubkeys: Vec<String>,
+    bls_signatures: Vec<String>,
+}
+
+
+#[derive(Debug)]
+pub struct PublicParameter {
+    pub g1: Vec<G1Affine>,
+    pub g2: Vec<G2Affine>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 pub fn read_user_data(path: &str) -> Result<Vec<Fr>> {
     let mut data = Reader::from_path(path)?;
@@ -51,4 +99,71 @@ pub fn read_user_data(path: &str) -> Result<Vec<Fr>> {
             Ok(Fr::from(big_int))
         })
         .collect()
+}
+
+
+
+
+pub fn srs(
+    path: &str,
+    transcript_index: usize
+) -> PublicParameter {
+    let json_string = fs::read_to_string(path)
+        .expect("Unable to read file");
+
+    let srs_json: SRSRaw = serde_json::from_str(&json_string)
+        .expect("JSON does not have correct format.");
+
+
+    // let g1 = srs_json
+    //     .transcripts[transcript_index]
+    //     .powers_of_tau
+    //     .g1_power
+    //     .iter()
+    //     .map(|g1_| Fr::try_from(g1_))
+    //     .collect::<Vec<G1Affine>>();
+
+    // let g2 = srs_json
+    //     .transcripts[transcript_index]
+    //     .powers_of_tau
+    //     .g2_power
+    //     .iter()
+    //     .map(|g2_| G2Affine::try_from(g2_))
+    //     .collect::<Vec<G2Affine>>();
+
+
+    PublicParameter {
+        g1: vec![],
+        g2: vec![]
+    }
+}
+
+// pub fn srs_g1() -> Vec<G1Affine> {
+//     vec![]
+// }
+
+// pub fn srs_g2() -> Vec<G2Affine> {
+//     vec![]
+// }
+
+
+
+#[cfg(test)]
+mod tests {
+    use ark_bn254::G1Affine;
+    use super::srs;
+    use super::*;
+
+
+    #[test]
+    fn test_build_srs() {
+        let pp = srs("./srs.json", 1);
+        let gg = Fr::from_str("0x97f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb");
+        match gg {
+            Ok(gg) => println!("{:?}", gg),
+            Err(e) => println!("{:?}", e)
+        }
+
+        println!("{:?}", gg);
+    }
 }
